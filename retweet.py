0ab5867ec708037b5e852fc3fc4182ed19b00f87
yogeshahuja21 <47299524+yogeshahuja21@users.noreply.github.com>
2019-06-13 20:48:41 +0530

    Add files via upload

---
+++ retweet.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+userid=pd.read_csv("C:/Users/Yogesh Kumar Ahuja/Desktop/ppts/Benign196DataSet/Benign196DataSet/userid.csv")
+tweets=pd.read_csv("C:/Users/Yogesh Kumar Ahuja/Desktop/ppts/Benign196DataSet/Benign196DataSet/tweets.csv")
+
